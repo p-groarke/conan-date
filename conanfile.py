1005ee0ce7a635aa99334b45d516967f7f5866d1
Roey Darwish Dror <roey.ghost@gmail.com>
2018-01-01 21:32:59 +0200

    Date 2.4

--- conanfile.py
+++ conanfile.py
@@ -6,59 +6,62 @@ import os
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
-    
+    name = "date"
+    version = "2.4"
+    url = "https://github.com/bincrafters/conan-date"
+    description = "A date and time library based on the C++11/14/17 <chrono> header "
+
     # Indicates License type of the packaged library
     license = "MIT"
-    
+
     # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-    
+
     # Remove following lines if the target lib does not use cmake.
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake" 
-    
-    # Options may need to change depending on the packaged library. 
+    generators = "cmake"
+
+    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    
+    options = {"shared": [True, False], "use_system_tz_db": [True, False],
+               "use_tz_db_in_dot": [True, False], }
+    default_options = ("shared=False", "use_system_tz_db=True", "use_tz_db_in_dot=False")
+
     # Custom attributes for Bincrafters recipe conventions
-    source_folder = "source_folder"
+    source_subfolder = "source_folder"
     build_folder = "build_folder"
-    
+
     # Use version ranges for dependencies unless there's a reason not to
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/[>=1.2.11]@conan/stable"
-    )
-        
+    def requirements(self):
+        if not self.options.use_system_tz_db:
+            self.requires("libcurl/[>=7.52.1]@bincrafters/stable")
+
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
+        source_url = "https://github.com/HowardHinnant/date"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
 
-        #Rename to "source_folder" is a convention to simplify later steps
-        os.rename(extracted_dir, self.source_folder)
+        #Rename to "source_subfolder" is a convention to simplify later steps
+        os.rename(extracted_dir, self.source_subfolder)
 
         # Helper method for common CMake configurations
-        wrap_cmake()
 
-        
+        self.wrap_cmake()
+
     def build(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTS"] = False # example
-        cmake.configure(source_folder=self.source_folder, build_folder=self.build_folder)
+        cmake.definitions["USE_SYSTEM_TZ_DB"] = self.options.use_system_tz_db
+        cmake.definitions["USE_TZ_DB_IN_DOT"] = self.options.use_tz_db_in_dot
+        cmake.definitions["BUILD_TZ_STATIC"] = not self.options.shared
+        cmake.configure(source_folder=self.source_subfolder, build_folder=self.build_folder)
         cmake.build()
         cmake.install()
-        
+
     def package(self):
         # If the CMakeLists.txt has a proper install method, the steps below may be redundant
         # If so, you can replace all the steps below with the word "pass"
-        include_folder = os.path.join(self.source_folder, "include")
+        include_folder = os.path.join(self.source_subfolder, "include")
         self.copy(pattern="LICENSE")
         self.copy(pattern="*", dst="include", src=include_folder)
         self.copy(pattern="*.dll", dst="bin", keep_path=False)
@@ -67,15 +70,15 @@ class LibnameConan(ConanFile):
         self.copy(pattern="*.so*", dst="lib", keep_path=False)
         self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
-        
+
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
 
-        
+
     # Helper method for common CMake configurations
     def wrap_cmake(self):
-        with tools.chdir(self.source_folder):
+        with tools.chdir(self.source_subfolder):
             os.rename("CMakeLists.txt", "CMakeListsOriginal.txt")
-        
-        cmake_wrapper_new = os.path.join(self.source_folder, "CMakeLists.txt")
+
+        cmake_wrapper_new = os.path.join(self.source_subfolder, "CMakeLists.txt")
         os.rename("CMakeLists.txt", cmake_wrapper_new)
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,10 @@
-#include <cstdlib>
 #include <iostream>
+#include <date/date.h>
+#include <date/tz.h>
 
 int main()
 {
-    std::cout << "Bincrafters\n";
+    auto& db = date::get_tzdb();
+    std::cout << date::weekday{date::jul/4/2001} << '\n';
     return EXIT_SUCCESS;
 }
